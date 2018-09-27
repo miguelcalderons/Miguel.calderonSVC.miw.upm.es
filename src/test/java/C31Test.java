fb0718d6d53a0945f48e23dcab66a35201a42bf5
Miguel Calderon <miguelcalderon10@gmail.com>
2018-09-27 23:14:48 +0200

    Crear la clase C31Test

---
+++ src/test/java/C31Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C31Test {
+
+    private C31 c31 = new C31();
+
+    @Test
+    void testM1(){
+        assertEquals("m1",this.c31.m1());
+    }
+
+    @Test
+    void testM2(){
+        assertEquals("m2",this.c31.m2());
+    }
+}
