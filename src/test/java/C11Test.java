ef26e999852e63f6672dfd539edcb26480652406
Miguel Calderon <miguelcalderon10@gmail.com>
2018-09-27 23:03:15 +0200

    Crear la clase C11Test

---
+++ src/test/java/C11Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class C11Test {
+
+    private C11 c11 = new C11();
+
+    @Test
+    void testM1(){
+        assertEquals("m1",this.c11.m1());
+    }
+
+    @Test
+    void testM2(){
+        assertEquals("m2",this.c11.m2());
+    }
+}
